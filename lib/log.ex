86bcfd8d56d178f0c0e9c6da07b76ec23e886148
Dhruv Jimulia <dj321@ic.ac.uk>
2024-02-14 20:27:26 +0000

    fix indentation in log.ex

--- lib/log.ex
+++ lib/log.ex
@@ -2,7 +2,7 @@
 # distributed algorithms, n.dulay, 14 jan 2024
 # coursework, raft consensus, v2
 
-defmodule Log do 
+defmodule Log do
 
 # implemented as a Map indexed from 1.
 
@@ -13,19 +13,19 @@ def new(server, log)          do Map.put(server, :log, log) end     # only used 
 def last_index(server)        do map_size(server.log) end
 def entry_at(server, index)   do server.log[index] end
 def request_at(server, index) do server.log[index].request end
-def term_at(_server, 0)       do 0 end 
+def term_at(_server, 0)       do 0 end
 def term_at(server, index)    do server.log[index].term end
 def last_term(server)         do Log.term_at(server, Log.last_index(server)) end
 
 def get_entries(server, range) do                 # e.g return server.log[3..5]
-  Map.take(server.log, Enum.to_list(range)) 
-  # equivalent to 
+  Map.take(server.log, Enum.to_list(range))
+  # equivalent to
   #   for k <- range.first .. range.last // 1, into: Map.new do {k, Log.entry_at(server, k)} end
 end
 
 def get_entries_from(server, from) do               # e.g return server.log[3..]
-  for k <- from .. Log.last_index(server) // 1, into: Map.new do 
-    {k, Log.entry_at(server, k)} 
+  for k <- from .. Log.last_index(server) // 1, into: Map.new do
+    {k, Log.entry_at(server, k)}
   end
 end
 
@@ -46,4 +46,3 @@ def delete_entries_from(server, from) do             # delete server.log[from..l
 end
 
 end # Log
-
